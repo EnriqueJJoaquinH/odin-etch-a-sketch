74cf5dd1d2dccedaad0527b670dd7e336d6f9994
enriquejh <enriquejjoaquinh@gmail.com>
2024-12-23 18:33:34 -0600

    Add a control panel and an erase option
    Using buttons on the website, the user can now switch between drawing
    or erasing when the cursor passes over the canvas. Each button has an
    event listener, and the 'moveover' event on canvas now is handled by a
    callback function named drawHandler

--- js/app.js
+++ js/app.js
@@ -1,20 +1,48 @@
+// ? Global variables
 let divisions = 16;
+let drawMode = 'paint';
 
+// ? DOM elements
 let canvas = document.querySelector('#canvas');
+let eraseBtn = document.querySelector('#erase-btn');
+let paintBtn = document.querySelector('#paint-btn');
 
-for (let row = 0; row < divisions; row++){
-    let newRow = document.createElement('div');
-    newRow.classList.add('row');
-    
-    for (let cell = 0; cell < divisions; cell++){
-        let newCell = document.createElement('div');
-        newCell.classList.add('cell');
-        newRow.appendChild(newCell);
+function createCanvasGrid() {
+    for (let row = 0; row < divisions; row++){
+        let newRow = document.createElement('div');
+        newRow.classList.add('row');
+
+        for (let cell = 0; cell < divisions; cell++){
+            let newCell = document.createElement('div');
+            newCell.classList.add('cell', 'erase');
+            newRow.appendChild(newCell);
+        }
+
+        canvas.appendChild(newRow);
     }
-    
-    canvas.appendChild(newRow);
 }
 
-canvas.addEventListener('mouseover', (event) => {
-    event.target.classList.add('paint');
+function drawHandler(event) {
+    switch (drawMode) {
+        case 'paint':
+            event.target.classList.remove('erase');
+            event.target.classList.add('paint');
+            break;
+        case 'erase':
+            event.target.classList.remove('paint');
+            event.target.classList.add('erase');
+            break;
+    }
+}
+
+createCanvasGrid()
+
+eraseBtn.addEventListener('click', () => {
+    drawMode = 'erase';
 });
+
+paintBtn.addEventListener('click', () => {
+    drawMode = 'paint';
+});
+
+canvas.addEventListener('mouseover', drawHandler);
